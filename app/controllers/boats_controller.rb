5f53c83d13bcd01e492599a3137b87df7d912fd2
esat1265 <mustafa.arslan2891@gmail.com>
2024-02-26 15:04:43 +0100

    created new page

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -2,4 +2,22 @@ class BoatsController < ApplicationController
   def index
     @boats = Boat.all
   end
+
+  def new
+    @boat = Boat.new
+  end
+
+  def create
+    @boat = Boat.new(boat_params)
+    if @boat.save
+      redirect_to boat_path(@boat)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def boat_params
+    params.require(:boat).permit(:name, :description, :price_per_day, :year_production, :photo)
+  end
 end
