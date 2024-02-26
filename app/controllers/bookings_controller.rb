e6bc671e87f60bbbcf75571f5743fcc8a73dcdcb
OliAivRo <rochat.olivia@gmail.com>
2024-02-26 17:17:34 +0100

    Modify the form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,18 @@
 class BookingsController < ApplicationController
+  before_action :set_boat, only: %i[new create]
+
   def new
+    @boatWanted = @boat
     @booking = Booking.new
+  end
+
+  def create
+
+  end
+
+  private
+
+  def set_boat
+    @boat = Boat.find(params[:boat_id])
   end
 end
