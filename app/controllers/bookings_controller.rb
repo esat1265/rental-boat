7cc1b89bf7e553d909715860ceacd08fa3000443
OliAivRo <rochat.olivia@gmail.com>
2024-02-27 10:09:08 +0100

    Save

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,17 +2,21 @@ class BookingsController < ApplicationController
   before_action :set_boat, only: %i[new create]
 
   def new
-    @boatWanted = @boat
+    @boat_to_book = @boat
     @booking = Booking.new
   end
 
   def create
-
+    @booking = Booking.new(booking_params)
   end
 
   private
 
   def set_boat
     @boat = Boat.find(params[:boat_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :price_per_day, :year_production, :photo)
   end
 end
