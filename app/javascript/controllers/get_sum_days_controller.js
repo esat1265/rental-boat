d3704fcfe3dc4004113d3f4a97b3bd4cab898016
OliAivRo <rochat.olivia@gmail.com>
2024-02-27 10:37:00 +0100

    Import flatpickr

---
+++ app/javascript/controllers/get_sum_days_controller.js
@@ -0,0 +1,8 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="get-sum-days"
+export default class extends Controller {
+  connect() {
+    console.log("LO");
+  }
+}
