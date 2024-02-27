44014367e254ae1452077b1b815c921f075487fe
esat1265 <mustafa.arslan2891@gmail.com>
2024-02-27 14:26:41 +0100

    just you can book a boat when you logged

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,5 @@
 class BookingsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, only: [:new, :create]
   before_action :set_boat, only: %i[new create]
 
   def new
