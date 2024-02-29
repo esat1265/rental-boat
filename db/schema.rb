3827ff6aaf0c2f3711366046f40574fce5c6d4e2
OliAivRo <rochat.olivia@gmail.com>
2024-02-29 13:55:09 +0100

    Add imgs for boats, and column longitude - latitude on boats table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_28_094622) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_29_125201) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +51,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_28_094622) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_boats_on_user_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,50 @@
 require "open-uri"
 require "pexels"
 
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+puts "Delete all datas"
+User.destroy_all
+
+puts "Prepare to create users"
+first_names = ["John", "Emma", "Michael", "Sophia", "William", "Olivia", "James", "Ava", "Benjamin", "Isabella"]
+last_names = ["Smith", "Johnson", "Williams", "Brown", "Jones", "Miller", "Davis", "Garcia", "Rodriguez", "Wilson"]
+
+puts "Create 30 users"
+30.times do
+  user = User.create(
+    email: Faker::Internet.email,
+    encrypted_password: "password"
+    first_name: first_names.sample,
+    last_name: last_names.sample
+  )
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 client = Pexels::Client.new(ENV['PEXELS_API_KEY'])
 
+puts "Destroy all datas"
 Booking.destroy_all
 Boat.destroy_all
 User.destroy_all
@@ -25,6 +57,7 @@ dummy_users = [
 ]
 
 # Create users from dummy data
+puts "Create the users"
 dummy_users.each do |user_data|
   User.create!(user_data)
 end
@@ -92,6 +125,7 @@ year_range = (2000..2024)
 
 # Create 10 boat records
 10.times do |i|
+  puts "Create #{i+1} boat"
   boat = Boat.new(
     name: boat_names.sample,
     description: boat_descriptions.sample,
@@ -103,6 +137,7 @@ year_range = (2000..2024)
   # Use a different keyword for each search
   keyword = keywords.sample
 
+  puts "Add a picture to #{i+1} boat"
   client.photos.search(keyword, per_page: 3).each_with_index do |photo, index|
     next unless photo.src&.has_key?('original')
 
